266fc59a0063294d21f808f64a812620a87cce66
etharalrehaili <85852139+etharalrehaili@users.noreply.github.com>
2025-05-06 12:27:55 +0300

    finish task

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Class Task',
+      title: 'Flutter Class Task',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
       ),
